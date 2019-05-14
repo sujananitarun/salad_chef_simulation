b67252d1306b43760860b05ecfd1001e7822c5c9
Tarun Sujanani <sujananitarun44@gmail.com>
2019-05-14 15:23:13 +0530

    Added Player Movement script to player 1 with arrow keys

---
+++ Assets/Scripts/player1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player1 : MonoBehaviour {
+
+    // Use this for initialization
+    public Rigidbody2D rb;
+    public float speed = 5;
+
+    void Move()
+    {
+        
+        rb.velocity = new Vector2(Input.GetAxis("Vertical") * speed, rb.velocity.y);
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.x);
+    }
+
+    void FixedUpdate () {
+        Move();
+    }
+    
+}
