ef3d9575ca1619366bcd4e09155ca40f95f73a34
Tarun Sujanani <sujananitarun44@gmail.com>
2019-05-15 14:21:50 +0530

    Added vegetable mats and eddited the script to give out debug statements on collide

--- Assets/Scripts/player1.cs
+++ Assets/Scripts/player1.cs
@@ -2,21 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player1 : MonoBehaviour {
+public class player1 : MonoBehaviour
+{
 
     // Use this for initialization
     public Rigidbody2D rb;
     public float speed = 5;
 
-    void Move()
+    private void OnCollisionEnter2D(Collision2D col)
     {
+
+        if (col.collider.tag == "LetterMat a")
+        {
+            Debug.Log("a");
+        }
+        else if (col.collider.tag == "LetterMat b")
+        {
+            Debug.Log("b");
+        }
         
-        rb.velocity = new Vector2(Input.GetAxis("Vertical") * speed, rb.velocity.y);
-        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.x);
     }
 
-    void FixedUpdate () {
+    
+
+    void Move()
+    {
+            rb.velocity = new Vector2(Input.GetAxis("Vertical") * speed, rb.velocity.y);
+            rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.x);
+        
+    }
+
+    void FixedUpdate()
+    {
         Move();
     }
-    
+
 }
---
+++ Assets/Scripts/player2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player2 : MonoBehaviour {
+
+    // Use this for initialization
+    public Rigidbody2D rb;
+    public float speed = 5;
+
+    void Move()
+    {
+        rb.velocity = new Vector2(Input.GetAxis("Fire2") * speed, rb.velocity.x);
+        rb.velocity = new Vector2(Input.GetAxis("Fire1") * speed, rb.velocity.x);
+    }
+
+    void FixedUpdate () {
+        Move();
+    }
+    
+}
